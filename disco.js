3326aa9c9f6334dd0cc329daace01443cfbbcd5e
rossyh95 <98370565+rossyh95@users.noreply.github.com>
2022-05-29 18:12:27 -0500

    Add files via upload

--- disco.js
+++ disco.js
@@ -7,28 +7,57 @@ class disco{
     }
 }
 
+discId = []
+for(let i=0;i<discId.length;i++);
+
+
 function addRow(){
 var discName = document.getElementById("name");
 var discPrice = document.getElementById("price");
 var discYear = document.getElementById("year");
 var table = document.getElementById("tableDisc");
-//var discId
-//for (var i = 0; i < discId.length; i++) {
-//        discId[i].innerHTML = (i+1)+".";
-//    }
+
 
 var rowCount = table.rows.length;
 var row = table.insertRow (rowCount);
 
-row.insertCell(0).innerHTML= //discId.value;
+
+// function crearDisco(name){
+// let iddisco = discId.length;
+// var discos = new disco(iddisco,name);
+// discId.push(discos);
+
+// }
+
+
+row.insertCell(0).innerHTML= discId.value;
 row.insertCell(1).innerHTML= discName.value;
 row.insertCell(2).innerHTML= discYear.value;
 row.insertCell(3).innerHTML= discPrice.value;
 row.insertCell(4).innerHTML= '<input type="button" value="Borrar" onClick="deleteRow(this)">';
 }
 
+
+//borrar
 function deleteRow(obj) {
     var index = obj.parentNode.parentNode.rowIndex;
     var table = document.getElementById("tableDisc");
     table.deleteRow(index);
 }
+
+//buscar
+var busqueda = document.getElementById('searchTerm');
+var table = document.getElementById("tableDisc").tBodies[0];
+
+buscaTabla = function(){
+    texto = busqueda.value.toLowerCase();
+    var r=0;
+    while(row = table.rows[r++])
+    {
+        if (row.innerText.toLowerCase().indexOf(texto) !== -1)
+        row.style.display = null;
+        else row.style.display = 'none';
+    }
+}
+
+busqueda.addEventListener('keyup', buscaTabla);
